4dee4c6896c1f34b0ae4c5275d12e2abe64a0277
sooyong <exceldra5@kaist.ac.kr>
2024-12-05 16:49:52 +0900

    save text information in one array

---
+++ include/Text.h
@@ -0,0 +1,23 @@
+// Text.h
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <deque>
+#include <mutex>
+#include <Settings.h>
+
+namespace text {
+
+
+// 텍스트 관리 클래스
+class Text
+{
+public:
+
+private:
+};
+}
+#endif // TEXT_H
